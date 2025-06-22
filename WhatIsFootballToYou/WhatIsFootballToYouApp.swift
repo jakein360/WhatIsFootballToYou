8fcdf17b287bc6f64f6d52244b0929c95914ce9d
Jake Emitte <jakeemitte829@gmail.com>
2025-06-22 12:32:31 -0700

    Initial Commit

---
+++ WhatIsFootballToYou/WhatIsFootballToYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootballToYouApp.swift
+//  WhatIsFootballToYou
+//
+//  Created by Jake Emitte on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootballToYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
